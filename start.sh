3088217616f559968035c9214fc9b48f777d9592
Felix Koppe <fkoppe@web.de>
2025-04-10 12:03:42 +0200

    Name container

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ mkdir -p output
 
 echo "🚀 Starting Coqui TTS server with CUDA..."
 
-docker run --rm \
+docker run --name coqui \
   --gpus all \
   -v coqui_cache_gpu:/root/.local/share/tts-gpu \
   -v "$(pwd)/output":/app/output \
